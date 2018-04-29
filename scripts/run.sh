6e08da50beb783df2875ab0c06d14b54c64c5ada
vafaei-ar <you@example.com>
2018-04-29 08:42:13 +0430

    flag

---
+++ scripts/run.sh
@@ -0,0 +1,19 @@
+#!/bin/env bash
+
+#SBATCH -J SCGAN
+#SBATCH --partition=dpt-gpu
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:pascal:1
+#SBATCH --mem=30000
+
+module load CUDA
+
+# see here for more samples:
+# /opt/cudasample/NVIDIA_CUDA-8.0_Samples/bin/x86_64/linux/release/
+
+# if you need to know the allocated CUDA device, you can obtain it here:
+echo $CUDA_VISIBLE_DEVICES
+
+srun tfpython $1 $2 $3
+
