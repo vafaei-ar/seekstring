6784a6c018dd01a75b390a70725d7371059954e1
gf <vafaei_ar@yahoo.com>
2018-08-22 22:00:06 +0430

    ng

--- scripts/toy_model/arch_0.py
+++ scripts/toy_model/arch_0.py
@@ -6,10 +6,13 @@ def architecture(x_in,drop_out):
     xp = tf.layers.batch_normalization(xp)
     xp = tf.nn.relu(xp)
 
-    xp = tf.layers.conv2d(x,filters=8,kernel_size=5,strides=(1, 1),padding='same')
+    xp = tf.layers.conv2d(xp,filters=8,kernel_size=5,strides=(1, 1),padding='same')
     xp = tf.layers.batch_normalization(xp)
     xp = tf.nn.relu(xp)
 
+    xp = tf.layers.conv2d(xp,filters=8,kernel_size=5,strides=(1, 1),padding='same')
+    xp = tf.layers.batch_normalization(xp)
+    xp = tf.nn.relu(xp)
 
     x1 = tf.layers.dropout(xp, drop_out)
     x_out = tf.layers.conv2d(x1,filters=1,kernel_size=5,strides=(1, 1),padding='same',
