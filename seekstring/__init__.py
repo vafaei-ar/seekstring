fe2a4cca23140535e3975f2c53aee05a02c7a2ca
gf <vafaei_ar@yahoo.com>
2018-08-31 02:51:40 +0430

    python3

--- seekstring/__init__.py
+++ seekstring/__init__.py
@@ -1,5 +1,9 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 modules = ['data_provider','networks','utils']
 
 for module in modules:
-	exec 'from '+module+' import *'
+	exec('from .'+module+' import *')
 
--- seekstring/toy_models.py
+++ seekstring/toy_models.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import sys
 import numpy as np
 from skimage.draw import polygon
